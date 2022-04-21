bc4787ac98b4293a7e008a38ee77f08b3d3ca30f
kachwanlan <62749627+kachwanlan@users.noreply.github.com>
2022-04-21 16:36:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+//First Assignment
+
+//First Assignment
+//First Assignment
+//First Assignment
+//Fifth Assignment
+let quote ="If you believe, nothing is impossible";
+document.getElementById("believe").innerHTML = quote;
+console.log(quote);
+//Sixth Assignment
+let x = "42" + 7;
+
+let y = "42" - 7;
+document.getElementById("answer1").innerHTML = 'x' +'=' + x;
+document.getElementById("answer2").innerHTML = 'y'+'='+y;
+console.log('x','=',x)
+console.log('y','=',y)
+//Seventh Assignment
+//Use the variable below 
+let coding1 = "cool";
+
+// Use another variable below 
+let coding2 = "trendy and fun";
+
+
+//Expected Output for number 1 //-> "coding is cool"
+console.log("coding is"+ " " + coding1);
+//Expected Output for number 2 //-> "coding is trendy and fun"
+console.log("coding is"+" "+coding2);
+//Eight Assignment
+let firstMeal = "Salad";
+
+let secondMeal = "Pasta";
+
+let thirdMeal = "Jollof Rice";
+
+let fourthMeal = "Ice Cream";
+
+console.log(firstMeal, secondMeal, thirdMeal, fourthMeal)
+
+
+//1. 
+let x1 = 31 + 7;
+// enter your guess where the '0' is on the next line
+let guessOne = 0;
+//WRITE ANSWER =38
+console.log(x1);
+
+//2.
+let y1 = 8;
+let food = "delicious";
+// Enter your guess on the next two lines, where the 'y' and 'food' is
+console.log(y1); //WRITE ANSWER = 8
+console.log(food); //WRITE ANSWER = delicious
+
+//3.
+let y12 = (9 * 6) / 3 + 2;
+// Enter your guess on the next line, where the '0' is
+let guessTwo = 0;//WRITE ANSWER = 38
+console.log(y12);
+
+//4.
+let z = "We learn to code for " + "a better future, better life, and better society.";
+// Enter your guess inside the "" below
+// Be careful! The text has to match exactly.
+let guessThree = "";//WRITE ANSWER = We learn to code for " + "a better future, better life, and better society.
+console.log(z);
+
